0376728fd6ba5e13c8b333c4648a0faf280e1371
Каштанов Александр Сергеевич <aleks-hd2015@yandex.ru>
2022-09-10 12:41:14 +0400

    homework 4

--- Program.cs
+++ Program.cs
@@ -1,97 +1,67 @@
-﻿/* Задача 19
-Напишите программу, которая принимает на вход пятизначное число и проверяет, 
-является ли оно палиндромом.
-14212 -> нет
-12821 -> да
-23432 -> да  */
-
- Console.WriteLine("Введите число");
-int number = Math.Abs(int.Parse(Console.ReadLine()));
-if (number >= 10000 && number <= 99999) {
-    Array ar = getArray(number);
-    Console.WriteLine(checkPalindrom(ar));
-} else {
-    Console.WriteLine("Введите 5ти значное число");
-}
-
-Array getArray(int number){
-   int num = number;
-   int[] array = new int[5];
-   for(int i = 0 ; i <= 4 ; i++){
-        array[i] = num % 10;
-        num = num / 10;
-    }
-    Array.Reverse(array);
-    return array;
-}
-
-String checkPalindrom(Array a){
-    int n = a.Length;
-    Boolean flag = false;
-    for (int i = 0; i <= n / 2; i++){
-        if (a.GetValue(i).Equals(a.GetValue(n-i-1))) {
-            flag = true;
-        } else {
-            flag = false;
-            break;
-        }
-    }
-    if (flag) {
-        return "ДА";
-    } else {
-        return "НЕТ";
-    }  
-} 
-
-/*
-Задача 21
-Напишите программу, которая принимает на вход координаты двух точек 
-и находит расстояние между ними в 3D пространстве.
-A (3,6,8); B (2,1,-7), -> 15.84
-A (7,-5, 0); B (1,-1,9) -> 11.53 */
-//Формируем первый массив
-int[] t1 = new int[3];
-int[] t2 = new int[3];
-try { 
-String[] dict = {"X","Y","Z"};
-for (int i = 0; i < dict.Length; i++){
-    Console.WriteLine($"Введите координату {dict[i]} для первой точки");
-    t1[i]= int.Parse(Console.ReadLine());      
-}
-for (int i = 0; i < dict.Length; i++){
-    Console.WriteLine($"Введите координату {dict[i]} для второй точки");
-    t2[i] = int.Parse(Console.ReadLine());
-}
-
-Console.Write(Math.Round(distance(t1,t2),2));
-Double distance(int[] t1, int[] t2){
-    Double x = Math.Pow((t2[0] - t1[0]),2);
-    Double y = Math.Pow((t2[1] - t1[1]),2);
-    Double z = Math.Pow((t2[2] - t1[2]),2);
-    return Math.Sqrt(x+y+z);
-}
-
- } catch (Exception e) {
-        Console.Write("Ввели не правильное число");
-    }
-
-/*
-Задача 23
-Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-3 -> 1, 8, 27
-5 -> 1, 8, 27, 64, 125
-*/
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и 
+возводит число A в натуральную степень B. Решить задачу с использованием методов.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
 try {
-    Console.WriteLine("Сколько нужно чисел ?");
-int n = int.Parse(Console.ReadLine());
-getPow(n);
-void getPow(int n){
-    for (int i = 1; i <= n; i ++){
-        Double result = Math.Pow(i,3); 
-        Console.Write($" {result},");  
-    } 
+    Console.WriteLine("Введите число: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите степень: ");
+    int n = int.Parse(Console.ReadLine());
+    natPow(a, n);
+} catch (Exception e) {
+    Console.Write("Ввели не правильное число");
 }
+void natPow(int a, int n){
+    int p = a;
+    for (int i = 1; i < Math.Abs(n); i++){
+        p = p * a;
+    }
+    Console.WriteLine(p);
+}
+/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */ 
+try {
+     Console.WriteLine("Введите число: ");
+    int a = Math.Abs(int.Parse(Console.ReadLine()));
+    getSumNumber(a);
 } catch (Exception e) {
     Console.Write("Ввели не правильное число");
 }
 
+void getSumNumber(int number){
+    int sum = 0;
+    int num = number;
+    while(num > 0){
+        sum += num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine(sum);
+}
+/*
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+ Элементы массива вводятся пользователем.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]  */
+getArray(setArray());
+int[] setArray(){
+    int[] array = new int[8];
+    for(int i = 0; i < 8; i++){
+        array[i] = new Random().Next(0,100);
+    }
+    return array;
+}
+void getArray(int[] array){
+    Console.Write("[");
+     for(int i =0; i<= array.Length-1; i++){
+       if (i != array.Length-1) {
+        Console.Write(array[i]+ ", ");
+       } else {
+        Console.Write(array[i]);
+       }
+    }
+    Console.Write("]");
+}  
