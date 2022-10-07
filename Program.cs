2c9ccb267ea09f5f9d6d178e0e3776b0bbeff26a
Каштанов Александр Сергеевич <aleks-hd2015@yandex.ru>
2022-10-08 00:44:03 +0400

    itog

--- Program.cs
+++ Program.cs
@@ -1,32 +1,83 @@
 ﻿/*
-Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
-y = k1 * x + b1, 
-y = k2 * x + b2; 
-
-значения b1, k1, b2 и k2 задаются пользователем.
-b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+/*
 try{
-Console.Write("Введите b1 ");
-double b1 = int.Parse(Console.ReadLine());
-Console.Write("Введите k1 ");
-double k1 = int.Parse(Console.ReadLine());
-Console.Write("Введите b2 ");
-double b2 = int.Parse(Console.ReadLine());
-Console.Write("Введите k2 ");
-double k2 = int.Parse(Console.ReadLine());
-double y = getY(k1, b1, k2, b2);
-double x = getX(y, b1, k1);
-Console.Write($"{x};{y}");
-} catch (Exception e){
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    natural(number);
+}catch (Exception e){
     Console.Write($"Ошибка {e.Message}");
 }
 
-double getY(double k1, double b1, double k2, double b2){
-    return (-k2*b1+b2*k1)/(k1-k2);
+void natural(int N){
+    if (N > 0){
+        Console.Write($"{N}, ");
+        natural(N-1);
+    }
+}
+*/
+/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+
+*/
+/*
+try{
+    Console.Write("Введите первое число: ");
+    int M = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе число: ");
+    int N = int.Parse(Console.ReadLine());
+    int result = getSumm(M,N);
+    Console.Write($"Сумма = {result}");
+}catch (Exception e){
+    Console.Write($"Ошибка {e.Message}");
 }
 
-double getX(double y, double b1, double k1){
-    return (y-b1)/k1;
+int getSumm(int M, int N){
+    int sum = 0;
+    for(int i = M; i <= N; i++){
+        sum += i;
+    }
+    return sum;
+}
+*/
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+/*
+try{
+    Console.Write("Введите первое положительное число: ");
+    int M = int.Parse(Console.ReadLine());
+    if (M < 0) {
+    throw new Exception("Первое число положительное отрицательное");
+    }
+    Console.Write("Введите второе число: ");
+    int N = int.Parse(Console.ReadLine());
+    if (N < 0) {
+    throw new Exception("Второе число отрицательное");
+    }
+
+    long result = getAcerman(M,N);
+    Console.Write($"Число Акермана = {result}");
+}catch (Exception e){
+    Console.Write($"Ошибка {e.Message}");
 }
 
+long getAcerman(int M, long N){
+    if (M == 0){
+       return ++N;
+    } else if (M > 0 && N == 0){
+        return getAcerman(M-1, 1);
+    } else if (M > 0 && N > 0){
+        return getAcerman(M - 1, getAcerman(M, N - 1));
+    } else return 1;
+}
+
+*/
